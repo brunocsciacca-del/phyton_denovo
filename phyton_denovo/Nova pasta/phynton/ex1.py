f356b3f10ea01562fbe773e10ba68b6994442a63
goalkeeperBruno <bruno.c.sciacca@gmail.com>
2026-09-22 09:36:17 -0300

    Add files via upload

---
+++ phyton_denovo/Nova pasta/phynton/ex1.py
@@ -0,0 +1,14 @@
+def calcularMediaGols (gols, pessoas):
+    total = 0
+    for gol in gols :
+        total += gol
+    return total
+def  calcularMediaGols(gols, pessoas):
+    media = 0
+    for gol in gols:
+        media += gol
+    return media/pessoas
+
+def emcontrarArtilheiro(gols, pessoas):
+
+gols = []
